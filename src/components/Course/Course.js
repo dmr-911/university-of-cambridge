8055024d6a6e8a2bb1611bcaf88da8dd9a225388
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-10-05 20:07:39 +0600

    course updated

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -9,28 +9,38 @@ const Course = (props) => {
     const chart = <FontAwesomeIcon icon={faChartBar} />;
     return (
       <Col>
-        <Card style={{overflow: 'hidden'}}>
-          <Row className="course-card" >
+        <Card style={{ overflow: "hidden" }}>
+          <Row className="course-card">
             <Col md={5}>
-            <Card.Img
-              height="150"
-              className="course-img"
-              variant="top"
-              src={picture}
-            />
+              <Card.Img
+                height="150"
+                className="course-img"
+                variant="top"
+                src={picture}
+              />
             </Col>
             <Col md={7}>
-            <Card.Body>
-              <Card.Title>{name}</Card.Title>
-              <Card.Text>Course Fee : {balance}</Card.Text>
-              <Card.Text>
-                <p>Contact Faculty</p>
-                Email : {email}
-                Phone : {phone}
-              </Card.Text>
-                        <Button variant="info" className="fw-bold text-white">{chart} Enroll Now</Button>{" "}
-              <Button variant="light" className="fw-bold border border-2 border-warning text-dark" disabled>40 Seats</Button>{" "}
-            </Card.Body>
+              <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>Course Fee : {balance}</Card.Text>
+                <Card.Text>
+                  <p>Contact Faculty</p>
+                  Email : {email}
+                </Card.Text>
+                <Card.Text>
+                  Phone : {phone}
+                </Card.Text>
+                <Button variant="info" className="fw-bold text-white">
+                  {chart} Enroll Now
+                </Button>{" "}
+                <Button
+                  variant="light"
+                  className="fw-bold border border-2 border-warning text-dark"
+                  disabled
+                >
+                  40 Seats
+                </Button>{" "}
+              </Card.Body>
             </Col>
           </Row>
         </Card>
